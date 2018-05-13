855af98fc7516c9f90ca40757e078639b79d95fa
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-13 09:11:29 -0500

    initial refactoring of survey page

--- app/public/survey_data.js
+++ app/public/survey_data.js
@@ -2,44 +2,7 @@
 console.log("we are linked")
 
 $(document).ready(function() {
-  // array of questions
-  var questions = 0,
-    options = 0
-  ;(surveyQuestions = [
-    `Is climate change real?`,
-    `Do you think astrological signs are important for a match?`,
-    `You enjoy helping others`,
-    `You think that Sundays are for brunch`,
-    `You enjoy laughter`,
-    `You prefer board games to bars`,
-    `You prefer museums over meet and greets`,
-    `An ideal saturday morning is binge watching Game of Thrones`,
-    `An ideal saturday morning is a sunny day at the beach`,
-    `You prefer outdoor adventure activities`
-  ]),
-    (scores = [])
 
-  console.log(surveyQuestions)
-
-  //  hiding modal so we only show it when submit button is clicked
-  // $("#myModal").modal('hide');
-
-  //for loop - question and question number
-  for (var i = 0, x = surveyQuestions.length; i < x; i++) {
-    questions++
-    $("form").append(`<h3> Question ${questions}`)
-    $("form").append(`<h4> ${surveyQuestions[i]}`)
-    $("form").append("<select>")
-  }
-  //  for loop - option selectors
-  for (var j = 1; j < 6; j++) {
-    $("select").append(
-      $("<option>", {
-        value: options[j],
-        text: j
-      })
-    )
-  }
 
   // event handler that adds score to score array when a value is selected
   $("select").on("change", function() {
