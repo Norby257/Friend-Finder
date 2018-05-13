6fae473e27fd68bcd0edd7bf670230199f25bfa1
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-13 16:16:59 -0500

    fixed html and API routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,12 +5,12 @@ module.exports = function(app) {
   //  get request to survey
   app.get("/survey", function(req, res) {
     //  render the survey page
-    res.sendFile(path.join(__dirname, "..", "/public", "/survey.html"))
+    res.sendFile(path.join(__dirname, "/../public/survey.html"))
   })
 
   //  catch all back to home page
-  app.get("/", function(req, res) {
+  app.get("*", function(req, res) {
     //send back the home page
-    res.sendFile(path.join(__dirname, "..", "/public", "/home.html"))
+    res.sendFile(path.join(__dirname, "/../public/home.html"))
   })
 }
